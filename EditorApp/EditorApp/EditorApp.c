0feaa55ca63b4a59676069b6deac112b8968a5b9
HP <heorhiiYvheniev@gmail.com>
2026-05-26 15:16:17 +0300

    created next functions: 1)start new line 2)save to file 3)load from file
    no updates on editor runner(yet)

--- EditorApp/EditorApp/EditorApp.c
+++ EditorApp/EditorApp/EditorApp.c
@@ -29,15 +29,93 @@ void Append(struct MemoStor* arr) {
     char *current_line = arr->lines[arr->lines_count - 1];
     int currentL_len = strlen(current_line);
     int new_len = strlen(input);
-    char *temp = realloc(arr->lines[arr->lines_count - 1], (currentL_len + new_len+1) * sizeof(char));
-    if (temp != NULL) {
-        arr->lines[arr->lines_count - 1] = temp;
+    char *tryy = realloc(arr->lines[arr->lines_count - 1], (currentL_len + new_len+1) * sizeof(char));
+    if (tryy != NULL) {
+        arr->lines[arr->lines_count - 1] = tryy;
         strcat(arr->lines[arr->lines_count - 1], input);
         printf("text appended successfully\n");
     }
     else {
         printf("Malloc failed\n");
     }
+}
+
+void start_new_line(struct MemoStor* arr) {
+    if (arr->lines_count >= arr->capacity) {
+        int new_capacity = arr->capacity *= 2;
+        char** tryy = realloc(arr->lines, new_capacity * sizeof(char*));
+        if (tryy == NULL) {
+            printf("malloc failed\n");
+            return;
+        }
+        arr->lines = tryy;
+        arr->capacity = new_capacity;
+    }
+
+    arr->lines[arr->lines_count] = (char*)malloc(1 * sizeof(char));
+    if (arr->lines[arr->lines_count] == NULL) {
+        printf("malloc failed\n");
+        return;
+    }
+    arr->lines[arr->lines_count][0] = '\0';
+    arr->lines_count++;
+    printf("new line started");
+}
+
+void save_to_file(struct MemoStor* arr) {
+    char filename[100];
+    printf("enter name for file to save text in(.txt)\n");
+    scanf("%s", filename);
+    FILE *file = fopen(filename, "w");
+    if (file == NULL) {
+        printf("file could not be opened\n");
+        return;
+    }
+    for (int i = 0; i < arr->lines_count; i++) {
+        fprintf(file, "%s\n", arr->lines[i]);
+    }
+    fclose(file);
+    printf("text saved to %s\n", filename);
+}
+
+void load_from_file(struct MemoStor* arr) {
+    char filename[100];
+    printf("enter name for file to load text from(.txt)");
+    scanf("%s", filename);
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("file could not be opened\n");
+        return;
+    }
+    char input[200];
+    while (fgets(input, sizeof(input), file) != NULL) {
+        input[strcspn(input, "\n")] = '\0';
+        if (arr->lines_count == 1 && arr->lines[0][0] == '\0') {
+            int str_len = strlen(input);
+            char* tryy = realloc(arr->lines[arr->lines_count - 1], (str_len + 1) * sizeof(char));
+            if (tryy != NULL) {
+                arr->lines[arr->lines_count - 1] = tryy;
+                strcat(arr->lines[arr->lines_count - 1], input);
+            }
+            else {
+                printf("Malloc failed\n");
+            }
+        }
+        else {
+            start_new_line(arr);
+            int str_len = strlen(input);
+            char* tryy = realloc(arr->lines[arr->lines_count - 1], (str_len + 1) * sizeof(char));
+            if (tryy != NULL) {
+                arr->lines[arr->lines_count - 1] = tryy;
+                strcat(arr->lines[arr->lines_count - 1], input);
+            }
+            else {
+                printf("Malloc failed\n");
+            }
+        }
+    }
+    fclose(file);
+    printf("text from file aploaded successfully\n");
 }
 void run_editor() {
     int choice;
